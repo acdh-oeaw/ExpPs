abe41ccefb2a5c46cbe83cb5b51bb042c148cbd7
csae8092 <p.andorfer@gmail.com>
2026-05-29 09:21:06 +0200

    feat(arche): adapts schema reference in teis for import -> filechecker can validate the files

--- arche/collect_binaries.py
+++ arche/collect_binaries.py
@@ -5,7 +5,6 @@ import os
 
 files = sorted(glob.glob("./xml/*/*.xml"))
 other_files = glob.glob("./schema/t*.*")
-print(other_files)
 all_files = files + other_files
 target_dir = "to_ingest"
 
@@ -15,3 +14,11 @@ for x in all_files:
     tail = os.path.split(x)[1]
     target = os.path.join(target_dir, tail)
     shutil.copy(x, target)
+
+for x in glob.glob(f"{target_dir}/*.xml"):
+    with open(x, "r", encoding="utf-8") as fp:
+        data = fp.read()# find and replace `../../schema/` with `''`
+    data = data.replace("../../schema/", '')
+    with open(x, "w", encoding="utf-8") as fp:
+        fp.write(data)
+
